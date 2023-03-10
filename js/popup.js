80eed18778133797a3ebdce789c0202a0e5ef2f1
Fernando Cedeño <bryan.cedeno01@epn.edu.ec>
2023-03-10 04:34:58 -0500

    Ferature: Se agregaron los videos, tabs

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,54 @@
-const play = document.querySelector(".fa-play");
-const ventanamodal=document.querySelector("popup-modal");
-play.onclick=function(){
-    ventanamodal.style.transform="scaleX(1)";
+const mas = document.getElementById("mas");
+const mas2 = document.getElementById("mas2");
+const menos = document.getElementById("menos");
+const menos2 = document.getElementById("menos2");
+
+mas.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        // Llamar a la función que se desea ejecutar
+        vermas('mas');
+    }
+});
+mas2.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        // Llamar a la función que se desea ejecutar
+        vermas2('mas');
+    }
+});
+menos.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        // Llamar a la función que se desea ejecutar
+        vermas('menos');
+    }
+});
+menos2.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        // Llamar a la función que se desea ejecutar
+        vermas2('menos');
+    }
+});
+
+
+
+
+
+function vermas(id){
+    if(id=="mas"){
+        document.getElementById("desplegar").style.display="block";
+        document.getElementById("mas").style.display="none";
+    }
+    else{
+        document.getElementById("desplegar").style.display="none";
+        document.getElementById("mas").style.display="inline";
+    }
 }
-play.onclick=function(){
-    ventanamodal.style.transform= "scaleX(0)";
+function vermas2(id){
+    if(id=="mas"){
+        document.getElementById("desplegar2").style.display="block";
+        document.getElementById("mas2").style.display="none";
+    }
+    else{
+        document.getElementById("desplegar2").style.display="none";
+        document.getElementById("mas2").style.display="inline";
+    }
 }
